58b9f9688706af376c1101b4857a816f30ee3fd9
Eric Burns <eric.michael.burns@gmail.com>
2011-03-12 11:03:57 -0500

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Eric Burns on 3/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController {
+    
+}
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,17 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Eric Burns on 3/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CalculatorTests : SenTestCase {
+@private
+    
+}
+
+@end
